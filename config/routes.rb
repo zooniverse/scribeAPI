09327efd2a17b6a343765843433dda66d40ba577
Stuart <stuart@zooniverse.org>
2014-04-03 17:50:14 -0400

    white space

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 API::Application.routes.draw do
+  
   root :to => "home#index"
+
   devise_for :users, :controllers => {:registrations => "registrations"}
 
   get '/workflows/:workflow_id/subjects' => 'subjects#index'
