3c5e9b30820411ca2d46bbbf0a02f79e036bd0b6
Stuart <stuart@zooniverse.org>
2014-04-03 17:50:07 -0400

    workflow serializer

--- app/serializers/workflow_serializer.rb
+++ app/serializers/workflow_serializer.rb
@@ -1,9 +1,8 @@
 class WorkflowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :tasks, :retire 
+  attributes :id, :label , :key , :tasks, :retire_limit, :first_task
 
   def id
     object._id.to_s
   end
-
 
 end
