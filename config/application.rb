ede87e92673f9c79bcaf59cdfcc8449b72fe2590
Stuart Lynn <stuartlynn@ITs-MacBook-Pro.local>
2014-04-02 22:07:31 -0400

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +13,18 @@ Bundler.require(*Rails.groups)
 
 module Scribe
   class Application < Rails::Application
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec
+      
+      
+      
+      
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
