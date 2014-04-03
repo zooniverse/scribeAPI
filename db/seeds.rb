8b57e56c435d2da5bf7e7ee17f77e9f6c5824ef8
Stuart <stuart@zooniverse.org>
2014-04-03 17:50:19 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,65 @@ Subject.destroy_all
 Workflow.destroy_all
 Classification.destroy_all
 
-p = Project.create
 
-verify_workflow 	 = Workflow.create({name: "verify", tasks:[]  , project: p })
-transcribe_workflow  = Workflow.create({name: "transcribe", tasks:[], enables_workflows: [verify_workflow.id], project: p })
-marking_workflow 	 = Workflow.create({name: "marking", enables_workflows: [transcribe_workflow.id], tasks:[{}], project: p })
+pages = [{ science:  """
+            <h1> Science Page</h1>
+             """,
+       about:    """
+            <h1> About Page</h1>
+             """,
+       library:  """
+            <h1> Library Page</h1>
+             """
+      }]
+
+p = Project.create(:pages => pages)
+
+marking_tasks = {
+      "drawSomething" => {
+        "type" => "drawing",
+        "question" => "Draw something.",
+        "choices" => [ {
+          "value" => "point",
+          "image" => "//placehold.it/30.png",
+          "label" => "Show Date",
+          "color" => "# "
+        },
+        {
+          "value" => "point",
+          "image" => "//placehold.it/30.png",
+          "label" => "Location",
+          "color" => "#ff0"
+        },
+        {
+          "value" => "point",
+          "image" => "//placehold.it/30.png",
+          "label" => "Cast Member",
+          "color" => "#ff0"
+        },
+        {
+          "value" => "point",
+          "image" => "//placehold.it/30.png",
+          "label" => "Production Staff",
+          "color" => "#ff0"
+        },
+        {
+          "value" => "point",
+          "image" => "//placehold.it/30.png",
+          "label" => "Thearter Name",
+          "color" => "#ff0"
+        }
+        ]
+    }
+  }
+
+# verify_workflow    = Workflow.create({name: "verify", tasks:[]  , project: p })
+transcribe_workflow  = Workflow.create({key: "transcribe", label:"Transcribe Contnet", tasks:{}, enables_workflows: {}, project: p })
+marking_workflow   = Workflow.create({key: "marking", label: "Mark Content", enables_workflows: {transcribe_workflow.id.to_s => {} }, tasks:marking_tasks, project: p })
+
+example_images = ["https://s3.amazonaws.com/programs-cropped.nypl.org/10/00261.jpg","https://s3.amazonaws.com/programs-cropped.nypl.org/10/00262.1.jpg","https://s3.amazonaws.com/programs-cropped.nypl.org/10/00262.2.jpg"]
+
 
 10.times do |i|
-	Subject.create(name:"subject_#{i}", url: "http://lorempixel.com/600/800/cats/", width: 600, height:800, workflows: [marking_workflow])
+  Subject.create(name:"subject_#{i}", url: example_images.sample, metadata: { width:504, height:782}, workflows: [marking_workflow])
 end
