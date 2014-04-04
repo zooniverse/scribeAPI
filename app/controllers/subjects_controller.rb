7b0cc6128689c56acee4983d58b1d6b55a6de63c
Stuart <stuart@zooniverse.org>
2014-04-04 15:01:47 -0400

    multiple subjects and removing root node from serilizer components

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -3,7 +3,7 @@ class SubjectsController < ApplicationController
 
   def index
   	workflow_id  = params["workflow_id"]
-  	respond_with  Subject.random(:workflow_ids => BSON::ObjectId.from_string(workflow_id) ).first
+  	respond_with  Subject.random(:workflow_ids => BSON::ObjectId.from_string(workflow_id), limit: (params[:limit].to_i || 5) )
   end
 
 end
