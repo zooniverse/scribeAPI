80356b373d9ba9185b07642146856bc150071520
Sascha T. Ishikawa <sascha@zooniverse.org>
2014-12-30 13:41:31 -0600

    try fix for asset precompiling

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,20 @@ module API
       g.helper_specs false
     end
 
+    initializer 'setup_asset_pipeline', :group => :all  do |app|
+      # We don't want the default of everything that isn't js or css, because it pulls too many things in
+      app.config.assets.precompile.shift
+
+      # Explicitly register the extensions we are interested in compiling
+      app.config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+          '.html', '.erb', '.haml',                 # Templates
+          '.png',  '.gif', '.jpg', '.jpeg',         # Images
+          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
+      end)
+    end
+
     config.browserify_rails.commandline_options = "-t coffee-reactify  --extension=\".cjsx\""
 
     config.middleware.use Rack::Cors do
