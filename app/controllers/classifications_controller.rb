1dc71a419968f9c57ca271069d13233a4c1aefef
Stuart <stuart@zooniverse.org>
2015-01-12 13:03:22 -0600

    json response to classification save

--- app/controllers/classifications_controller.rb
+++ app/controllers/classifications_controller.rb
@@ -1,14 +1,15 @@
 class ClassificationsController < ApplicationController
+  respond_to :json
 
   def new
   end
 
-  def create 
+  def create
   	annotations = params["annotations"]
   	subject_id  = BSON::ObjectId.from_string params["subject_id"]
   	workflow_id = BSON::ObjectId.from_string params["workflow_id"]
   	user_id     = BSON::ObjectId.from_string params["user_id"]
-
-  	Classification.create(workflow_id: workflow_id, subject_id: subject_id, user_id: user_id)
+  	@result     = Classification.create(workflow_id: workflow_id, subject_id: subject_id, user_id: user_id)
+    respond_with @result
   end
 end
