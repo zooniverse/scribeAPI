0c234ef3cd3aa6df58dcfc52aa0e3ed3f814b02f
Stuart <stuart@zooniverse.org>
2015-02-23 13:43:26 -0600

    project description

---
+++ project/example_project/project.rb
@@ -0,0 +1,38 @@
+project = {
+  title: "Animals vs Cats",
+  summary: "We want to see if (yes obviously) cats are better than animals",
+  team: [
+    {name: "Stuart Lynn",
+     role: "Cat lover",
+     image: "http://stuart.com/stuart.jpg",
+     organization: "zooniverse",
+
+     },
+
+    {name: "Sascha Ishikawa",
+      role: "Cat",
+      image: "http://sascha.com/cat.jpg",
+      organization: "zooniverse",
+    },
+
+    {name: "Andrea Simenstad",
+      role: "Not a cat(probably)",
+      image: "http://Andrea.com/Andrea.jpg",
+      organization: "UofM",
+    }
+  ]
+  organizations:[
+    {
+     title: "Zooniverse",
+     description: "Zooniversing the universe one zoo at a time",
+     url:"http://zooniverse.org",
+     logo: "http://zooniverse.org/logo.png"
+    },
+    {
+     title: "UofM",
+     description: "A very cold university",
+     url:"http://umn.edu",
+     logo: "http://umn.edu/logo.png"
+    }
+  ]
+}
