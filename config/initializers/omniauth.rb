cb4b996955812931de081d08d316f3d56f51c09d
Stuart <stuart@zooniverse.org>
2015-03-31 17:26:53 -0400

    moving omniauth setup in to devise initializer

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-
-  providers = YAML.load_file(Rails.root.join('config', 'login_providers.yml'))
-
-  provider :google_oauth2, providers["google"]["id"], providers["google"]["secret"] if providers["google"]
-  provider :omniauth, :facebook, providers["facebook"]["id"], providers["facebook"]["secret"]  if providers["facebook"]
-
-end
