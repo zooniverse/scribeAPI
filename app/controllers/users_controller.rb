d27ed96a9381efcc3d42c39a5de2965884c2b8f0
Stuart <stuart@zooniverse.org>
2015-04-02 17:00:15 -0400

    simpler user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,8 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
   respond_to :json
-
-
-  def index
-    @users = User.all
-    respond_with @users
-  end
-
-  def show
-    @user = User.find(params[:id])
-    respond_with @user
-  end
 
   def logged_in_user
     respond_with current_user
   end
-
-
 
 end
