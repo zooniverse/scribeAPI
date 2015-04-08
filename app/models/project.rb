aaffe9c2509835ed093ab1f7887ce2a28b9ca555
Brian Foo <brian@youaremyjoy.org>
2015-04-08 18:01:48 -0400

    fixed whitespace/formatting in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -23,72 +23,72 @@ class Project
 	has_many :subjects
 
 	def calc_stats
-	  # amount of days to calculate statistics for
-	  range_in_days = 60
-	  datetime_format = "%Y-%m-%d %H:00"
-	  
-	  # determine date range
-	  current_time = Time.new
-	  end_date = current_time
-	  start_date = end_date - range_in_days.days
-	  
-	  # calculate total counts
-	  total_users = User.count
-	  total_subjects = Subject.count
-	  total_classifications = Classification.count
-	  
-	  # retrieve user data in range
-	  users_data = []    
+    # amount of days to calculate statistics for
+    range_in_days = 60
+    datetime_format = "%Y-%m-%d %H:00"
+    
+    # determine date range
+    current_time = Time.new
+    end_date = current_time
+    start_date = end_date - range_in_days.days
+    
+    # calculate total counts
+    total_users = User.count
+    total_subjects = Subject.count
+    total_classifications = Classification.count
+    
+    # retrieve user data in range
+    users_data = []     
     users_in_range = User.where(:created_at => start_date..end_date).group_by {|d| d.created_at.strftime(datetime_format)}                          
     (start_date.to_i..end_date.to_i).step(1.hour) do |i_date|
       n_date = Time.at(i_date)
       hour = n_date.strftime(datetime_format)
       users_data << {
-        date: hour,
-        value: users_in_range[hour] ? users_in_range[hour].size : 0
+      date: hour,
+      value: users_in_range[hour] ? users_in_range[hour].size : 0
       }
     end
     
     # retrieve subject data
     subjects_data = []    
-    subject_groups = Subject.all.group_by {|d| d.state}    
+    subject_groups = Subject.all.group_by {|d| d.state}   
     subject_groups.each do |state, subjects|
       subjects_data << {
-        label: state,
-        value: subjects.size
+      label: state,
+      value: subjects.size
       }
     end
     
     # retrieve classification data in range
-    classifications_data = []    
+    classifications_data = []   
     classifications_in_range = Classification.where(:created_at => start_date..end_date).group_by {|d| d.created_at.strftime(datetime_format)}                          
     (start_date.to_i..end_date.to_i).step(1.hour) do |i_date|
       n_date = Time.at(i_date)
       hour = n_date.strftime(datetime_format)
       classifications_data << {
-        date: hour,
-        value: classifications_in_range[hour] ? classifications_in_range[hour].size : 0
+      date: hour,
+      value: classifications_in_range[hour] ? classifications_in_range[hour].size : 0
       }
     end
     
     # return data
-		{
-		  updated_at: current_time.strftime(datetime_format),
-		  start_date: start_date.strftime(datetime_format),
+    {
+      updated_at: current_time.strftime(datetime_format),
+      start_date: start_date.strftime(datetime_format),
       end_date: current_time.strftime(datetime_format),
-		  users: {
-		    count: total_users,
-		    data: users_data
-		  },
-		  subjects: {
-        count: total_subjects,
-        data: subjects_data      
+      users: {
+        count: total_users,
+        data: users_data
       },
-		  classifications: {
+      subjects: {
+        count: total_subjects,
+        data: subjects_data     
+      },
+      classifications: {
         count: total_classifications,
         data: classifications_data     
       }
-		}
-	end
+    }
+  end
 
 end
