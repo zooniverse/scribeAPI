8bc4fd64fc70a195c29ae2d8a35a9443e3dc3816
simensta <andrea@zooniverse.org>
2015-04-20 13:14:03 -0500

    revised classification model to :triggered_followup_subjects, class_name: 'Subject'

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -15,11 +15,12 @@ class Classification
   belongs_to :workflow
   belongs_to :user
   belongs_to :subject
-  has_many   :triggered_followup_subjects, as: :subject
+  has_many   :triggered_followup_subjects, class_name: "Subject"
 
   before_create :generate_new_subjects
 
   def generate_new_subjects
+    binding.pry
     if workflow.generates_new_subjects
       triggered_followup_subject_ids = workflow.create_follow_up_subjects(self)
     end
