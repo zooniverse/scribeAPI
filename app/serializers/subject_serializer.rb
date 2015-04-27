fa309b49cfd2d80015a22986bdc65844d304b0c1
simensta <andrea@zooniverse.org>
2015-04-27 12:09:09 -0500

    SubjectSerializer returns :child_subjects_info which contains information for re-creating secondary subjects on the mark-index

--- app/serializers/subject_serializer.rb
+++ app/serializers/subject_serializer.rb
@@ -1,5 +1,5 @@
 class SubjectSerializer < ActiveModel::MongoidSerializer
-  attributes :id, :parent_subject_id, :workflow_id, :name, :location, :classification_count, :child_subjects, :meta_data, :user_favourite
+  attributes :id, :parent_subject_id, :workflow_id, :name, :location, :classification_count, :child_subjects_info, :meta_data, :user_favourite
   delegate :current_user, to: :scope
 
   def id
@@ -10,8 +10,23 @@ class SubjectSerializer < ActiveModel::MongoidSerializer
     (scope and scope.has_favourite?(object))
   end
 
-  def child_subjects
-    object.child_subjects
+  def child_subjects_info
+    child_subjects = object.child_subjects
+    child_subject_info = []
+    child_subjects.each do |child|
+
+      rebuild_info = {
+        id: child.id,
+        uri: child.location["uri"],
+        relationship_to_parent: child.location["spec"],
+        tool_type: child.tool_task_description["type"],
+        mark_label: child.tool_task_description["label"]
+      }
+
+      child_subject_info << rebuild_info
+      
+    end
+    child_subject_info
   end
 
 end
