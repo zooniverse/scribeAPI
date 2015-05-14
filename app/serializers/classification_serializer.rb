c0aeb82a6cbf8b5968d4d71bfde8a3989ae34982
simensta <andrea@zooniverse.org>
2015-05-14 16:51:59 -0500

    ClassificationSerializer now returns entire generated subject

--- app/serializers/classification_serializer.rb
+++ app/serializers/classification_serializer.rb
@@ -1,12 +1,28 @@
 class ClassificationSerializer < ActiveModel::MongoidSerializer
   attributes :id, :workflow_id, :subject_id, :subject_set_id, :location, :annotations, :triggered_followup_subject_ids, :child_subject_id, :child_subject
   
-  belongs_to :workflow
-  belongs_to :user
-  belongs_to :subject
+  has_one :workflow
+  has_one :user
+  has_one :subject
 
   def id
     object._id.to_s
+  end
+
+  def workflow_id
+    object.workflow_id.to_s
+  end
+
+  def subject_id
+    object.subject_id.to_s
+  end
+
+  def subject_set_id
+    object.subject_set_id.to_s
+  end
+
+  def child_subject_id
+    object.child_subject_id.to_s
   end
 
   def child_subject
