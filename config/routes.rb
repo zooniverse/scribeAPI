a11ed97c1d3ab97a14d9cdb6a13ac34a8ab33363
simensta <andrea@zooniverse.org>
2015-06-25 17:28:14 -0500

    add route GET /subject_sets/:subject_set_id/subjects/:subject_id

--- config/routes.rb
+++ config/routes.rb
@@ -17,16 +17,17 @@ API::Application.routes.draw do
   end
   get '/admin' => 'admin/subjects#index'
 
-  get  '/current_user',                       to: "users#logged_in_user"
-  get '/dashboard/ancestory',                 to: 'dashboard#ancestory'
-  get '/dashboard',                           to: 'dashboard#index'
-  get '/projects/stats',                      to: 'projects#stats'
-  get '/project/style.css',                   to: 'projects#project_css',  defaults: { format: 'css' }
-  get '/workflows/:workflow_id/subjects',     to: 'subjects#index'
-  get '/workflows/:workflow_id/subject_sets', to: 'subject_sets#index'
-  get '/subjects/:subject_id',                to: 'subjects#show',         defaults: { format: 'json' }
-  get '/subject_sets/:subject_set_id',        to: 'subject_sets#show',     defaults: { format: 'json' }
-  get '/classifications/terms/:workflow_id/:annotation_key', to: 'classifications#terms'
+  get  '/current_user',                                       to: "users#logged_in_user"
+  get '/dashboard/ancestory',                                 to: 'dashboard#ancestory'
+  get '/dashboard',                                           to: 'dashboard#index'
+  get '/projects/stats',                                      to: 'projects#stats'
+  get '/project/style.css',                                   to: 'projects#project_css',  defaults: { format: 'css' }
+  get '/workflows/:workflow_id/subjects',                     to: 'subjects#index'
+  get '/workflows/:workflow_id/subject_sets',                 to: 'subject_sets#index'
+  get '/subjects/:subject_id',                                to: 'subjects#show',         defaults: { format: 'json' }
+  get '/subject_sets/:subject_set_id',                        to: 'subject_sets#show',     defaults: { format: 'json' }
+  get '/subject_sets/:subject_set_id/subjects/:subject_id',    to: 'subject_sets#show',     defaults: { format: 'json' }
+  get '/classifications/terms/:workflow_id/:annotation_key',  to: 'classifications#terms'
 
   post   '/subjects/:id/favourite',           to: 'favourites#create',     defaults: { format: 'json' }
   post   '/subjects/:id/unfavourite',         to: 'favourites#destroy',    defaults: { format: 'json' }
