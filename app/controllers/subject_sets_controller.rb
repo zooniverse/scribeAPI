f92111b9bf3492b5263a495041f156a058f27512
simensta <andrea@zooniverse.org>
2015-06-30 11:45:40 -0500

    added :selected_subject_id attribute to subject_set_serializer

--- app/controllers/subject_sets_controller.rb
+++ app/controllers/subject_sets_controller.rb
@@ -5,7 +5,6 @@ class SubjectSetsController < ApplicationController
     workflow_id  = params["workflow_id"]
     random = params["random"] || false
     limit  = params["limit"].to_i  || 10
-    subject_request_scope = params["subject_request_scope"]
     
     if  workflow_id
       query = {"counts.#{workflow_id}.active_subjects" => {"$gt"=>0}}
@@ -22,19 +21,20 @@ class SubjectSetsController < ApplicationController
     # sets = sets.sort_by { |s| - s.subjects.first.secondary_subject_count }
 
     # Randomizer#random seems to want query criteria passed in under :selector key:
-    respond_with sets, each_serializer: SubjectSetSerializer, workflow_id: workflow_id, limit: limit, random: random, subject_request_scope: subject_request_scope
+    respond_with sets, each_serializer: SubjectSetSerializer, workflow_id: workflow_id, limit: limit, random: random
   end
 
   # DOES NOT APPEAR TO BE IN USE -STI
   def show
     limit = 1 # should only return one (the matched set)
+    subject_id = params[:subject_id]
     # limit = params["limit"].to_i || 10
     puts 'SUBJECT SET ID: ', params[:subject_set_id]
     set = SubjectSet.where(id: params[:subject_set_id])
     workflow_id  = params["workflow_id"]
 
     return render status: 404, json: {status: 404} if set.nil?
-    respond_with set, status: (set.nil? ? :not_found : 201), each_serializer: SubjectSetSerializer, workflow_id: workflow_id, limit: limit
+    respond_with set, status: (set.nil? ? :not_found : 201), each_serializer: SubjectSetSerializer, workflow_id: workflow_id, subject_id: subject_id, limit: limit
   end
 
 end
--- app/serializers/subject_set_serializer.rb
+++ app/serializers/subject_set_serializer.rb
@@ -1,10 +1,14 @@
 class SubjectSetSerializer < ActiveModel::MongoidSerializer
 
-  attributes :id, :name, :thumbnail, :meta_data, :subjects, :state, :counts, :group_id
+  attributes :id, :name, :thumbnail, :meta_data, :subjects, :state, :counts, :group_id, :selected_subject_id
   has_many :subjects
 
   def id
     object._id.to_s
+  end
+
+  def selected_subject_id
+    serialization_options[:subject_id] if serialization_options[:subject_id]
   end
 
   def subjects
@@ -14,19 +18,12 @@ class SubjectSetSerializer < ActiveModel::MongoidSerializer
 
     return nil if object.nil?
 
-    if serialization_options[:subject_request_scope] == "active_root"
-      if random
-        object.subjects.active_root.where(workflow_id: workflow_id).random(limit: limit)
-      else
-        object.subjects.active_root.where(workflow_id: workflow_id).limit(limit)
-      end
-    elsif serialization_options[:subject_request_scope] == "active"
-      if random
-        object.subjects.active.where(workflow_id: workflow_id).random(limit: limit)
-      else
-        object.subjects.active.where(workflow_id: workflow_id).limit(limit)
-      end
+    if random
+      subjects = object.subjects.active_root.where(workflow_id: workflow_id).random(limit: limit)     
+    else
+      subjects = object.subjects.active_root.where(workflow_id: workflow_id).limit(limit)
     end
+
   end
 
 end
