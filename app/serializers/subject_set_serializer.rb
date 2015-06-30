7faf839d460b1e83c55b01fc50d2c4b3b4cbf40b
simensta <andrea@zooniverse.org>
2015-06-30 15:59:12 -0500

    removed random from subject_set_serializer query, ensured subjects are being processed through the subject_serializer

--- app/serializers/subject_set_serializer.rb
+++ app/serializers/subject_set_serializer.rb
@@ -5,7 +5,7 @@ class SubjectSetSerializer < ActiveModel::MongoidSerializer
   def attributes
     data = super
     data[:pagination_info] = data[:subjects][:pagination_info]
-    data[:subjects] = data[:subjects][:subjects]
+    data[:subjects] = data[:subjects][:subjects].map {|s| SubjectSerializer.new(s, root: false)}
     data
   end
 
@@ -14,7 +14,7 @@ class SubjectSetSerializer < ActiveModel::MongoidSerializer
   end
 
   def subjects
-    workflow_id = serialization_options[:workflow_id]
+    workflow_id = serialization_options[:workflow_id] # I think we need to pull this from serialization_options, subject_sets don't belong to workflow.
     limit = serialization_options[:limit].to_i
     random = serialization_options[:random]
 
@@ -22,9 +22,9 @@ class SubjectSetSerializer < ActiveModel::MongoidSerializer
 
     # we will have to send the page through serialization_options
     if random
-      subjects = Kaminari.paginate_array(object.subjects.active_root.where(workflow_id: workflow_id).random(limit: limit)).page(serialization_options[:page])      
+      subjects = object.subjects.active_root.where(workflow_id: workflow_id).page(serialization_options[:page])      
     else
-      subjects = Kaminari.paginate_array(object.subjects.active_root.where(workflow_id: workflow_id).limit(limit)).page(serialization_options[:page])
+      subjects = object.subjects.active_root.where(workflow_id: workflow_id).limit(limit).page(serialization_options[:page])
     end
 
     subjects = {
@@ -37,6 +37,7 @@ class SubjectSetSerializer < ActiveModel::MongoidSerializer
         total_pages: subjects.total_pages,
       }
     }
+
   end
 
 end
