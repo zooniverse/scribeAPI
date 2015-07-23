f080b665d6350e999d10481918b45031bcb7855d
Paul Beaudoin <contact@paulbeaudoin.com>
2015-07-23 15:57:29 -0400

    misconfigured oauth providers were not disappearing from header, fixed

--- config/initializers/register_auth_options.rb
+++ config/initializers/register_auth_options.rb
@@ -1,4 +1,13 @@
 Rails.application.config.before_initialize do
   providers = YAML.load(ERB.new(File.read(Rails.root.join('config', 'login_providers.yml.erb'))).result)["providers"]
-  API::Application.config.auth_providers = providers.keys
+
+  provider_keys = []
+  providers.each do |k,v| 
+    if ! v['id'].blank? && ! v['secret'].blank?
+      provider_keys << k
+    end
+  end
+
+  API::Application.config.auth_providers = provider_keys
+  puts "Init: OAUTH providers: #{API::Application.config.auth_providers}"
 end
