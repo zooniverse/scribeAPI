e30c152f9f3636d24d3a591ae372020d12dd4966
simensta <andrea@zooniverse.org>
2015-10-02 14:44:53 -0500

    enabled compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 API::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
+  config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
