53781afa7212f56f2640ad1a29763dd75447182f
Sascha T. Ishikawa <sascha@zooniverse.org>
2015-12-04 11:09:22 -0600

    Revert "properly save hash (for realz now)"
    This reverts commit 3e94ea62885bd146371b591761ed343b10073945.

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,7 @@ class User
   field :tutorial_complete,  :type => Boolean, :default => false
 
   # for remembering users' last viewed subject
-  field :subject_set_bookmarks, :type => Hash, :default => {}
+  field :subject_set_bookmarks, :type => Hash
 
   has_many :favourites
   has_many :classifications
@@ -67,8 +67,7 @@ class User
   end
 
   def set_subject_bookmark(subject_set_id,page)
-    subject_set_id = subject_set_id.to_s
-    self.subject_set_bookmarks[subject_set_id] = page
+    self.subject_set_bookmarks = {subject_set_id.to_s => page}
     save!(:validate => false)
     # update_attribute subject_set_bookmarks[:subject_set_id] = page
     puts 'BOOKMARKS = ', self.subject_set_bookmarks
