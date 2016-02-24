49ad2997b2ad4a5687f3955ce1c14d20148d805a
simensta <andrea@zooniverse.org>
2016-02-24 09:45:34 -0600

    GA tag fix

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::MongoidSerializer
-  attributes :id, :title, :short_title, :summary, :home_page_content, :organizations , :team, :pages, :menus, :partials, :logo, :background, :workflows, :forum, :tutorial, :feedback_form_url, :metadata_search, :terms_map, :blog_url, :discuss_url, :privacy_policy
+  attributes :id, :title, :short_title, :summary, :home_page_content, :organizations , :team, :pages, :menus, :partials, :logo, :background, :workflows, :forum, :tutorial, :feedback_form_url, :metadata_search, :terms_map, :blog_url, :discuss_url, :privacy_policy, :analytics
   has_many :workflows
 
   # delegate :current_or_guest_user, to: :scope
