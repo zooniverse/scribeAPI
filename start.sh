adc48613a8129881c4d0ba4c36424b51f99fb747
Adam McMaster <adam@zooniverse.org>
2018-01-11 11:56:20 +0000

    Load environment and config from docker secrets

--- start.sh
+++ start.sh
@@ -6,6 +6,16 @@ then
     exit 1
 fi
 
+if [ -f /run/secrets/environment ]
+then
+  source /run/secrets/environment
+fi
+
+if [ ! -d /config/ ]
+then
+    ln -s /run/secrets/ /config
+fi
+
 ln -fs /config/* ./config/
 
 env_configs="./config/env_vars.sh"
