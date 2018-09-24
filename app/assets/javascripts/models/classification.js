ae296ccb192013ccb9d6a04d957d0ed059823511
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-09-24 17:20:41 +0200

    Clean up of Classification

--- app/assets/javascripts/models/classification.js
+++ app/assets/javascripts/models/classification.js
@@ -1,9 +1,4 @@
-/*
- * decaffeinate suggestions:
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-class Classification {
+export default class Classification {
   constructor() {
     this.metadata = {
       started_at: (new Date).toISOString()
@@ -13,10 +8,6 @@ class Classification {
     this.subject_set_id = null;
     this.task_key = null;
 
-    this.annotation = {};
-    this.subject_id = null;
-    this.subject_set_id = null;
-    this.task_key = null;
     this.generates_subject_type = null;
     this.toolName = null;
     this.committed = false;
@@ -42,50 +33,18 @@ class Classification {
       }
     };
 
-    return $.ajax("/classifications", {
+    $.ajax("/classifications", {
       data: data,
       method: 'post',
       dataType: 'json',
       complete: (resp) => {
-        return typeof callback === "function"
-          ? callback(
-            resp.responseJSON != null
-              ? resp.responseJSON.classification
-              : undefined
-          )
-          : undefined;
+        typeof callback === "function" ?
+          callback(
+            resp.responseJSON != null ?
+            resp.responseJSON.classification :
+            undefined
+          ) : undefined;
       }
     });
   }
 }
-
-// $.ajax('/classifications', data: data, method: 'post', dataType: 'json').done((response)->
-//   console.log 'success: ', response
-//   callback? response.responseJSON?.classification
-//   return
-// ).fail(->
-//   console.log 'error'
-//   return
-// ).always(->
-//   console.log 'complete (always)'
-//   return
-// )
-
-// console.log 'Classification::commit() END'
-// # Perform other work here ...
-// # Set another completion function for the request above
-// jqxhr.always ->
-//   alert 'second complete'
-//   return
-
-// rec.save()
-// rec = API.type('classifications').create
-//   annotation: @annotation
-//   subject_id: @subject_id
-//   subject_set_id: @subject_set_id
-//   task_key: @task_key
-//   metadata: @metadata
-//   workflow_id: @workflow_id
-// rec.save()
-
-export default Classification;
