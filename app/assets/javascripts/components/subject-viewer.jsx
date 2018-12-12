067777078a2b732e285a205afc682eca335fe522
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-12-12 15:10:41 +0100

    Fixed regression in subject viewer after conversion to ES6 component

--- app/assets/javascripts/components/subject-viewer.jsx
+++ app/assets/javascripts/components/subject-viewer.jsx
@@ -496,7 +496,7 @@ export default class SubjectViewer extends React.Component {
                     isPriorMark={isPriorMark}
                     subjectCurrentPage={this.props.subjectCurrentPage}
                     selected={mark === this.state.selectedMark}
-                    getEventOffset={this.getEventOffset}
+                    getEventOffset={this.getEventOffset.bind(this)}
                     submitMark={this.submitMark}
                     sizeRect={this.refs.sizeRect}
                     displaysTranscribeButton={displaysTranscribeButton}
@@ -556,9 +556,9 @@ export default class SubjectViewer extends React.Component {
             height={this.props.subject.height}
           />
           <MouseHandler
-            onStart={this.handleInitStart}
-            onDrag={this.handleInitDrag}
-            onEnd={this.handleInitRelease}
+            onStart={this.handleInitStart.bind(this)}
+            onDrag={this.handleInitDrag.bind(this)}
+            onEnd={this.handleInitRelease.bind(this)}
             inst="marking surface"
           >
             <SVGImage
@@ -590,7 +590,7 @@ export default class SubjectViewer extends React.Component {
                       yScale={scale.vertical}
                       disabled={isPriorMark}
                       selected={mark === this.state.selectedMark}
-                      getEventOffset={this.getEventOffset}
+                      getEventOffset={this.getEventOffset.bind(this)}
                       ref={this.refs.sizeRect}
                       onSelect={this.selectMark.bind(
                         this,
